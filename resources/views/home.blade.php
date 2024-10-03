33a55f1cde61ae30b68418a74a94576cbfdea1ce
mhamad <magody.ibrahem3522930@gmail.com>
2024-10-03 11:06:48 +0300

    27 commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- Form 1 --}}
+    {{-- <my-button></my-button> --}}
+    {{-- Form 2 --}}
+    <my-button text = "My New Text Button" type="submit"></my-button>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CustomersController;
 use App\Http\Controllers\ContactFormController;
 use App\Http\Controllers\HomeController;
 Route::view('/', 'home');
 Route::view('about' , 'about' );
-//For the resource it auto-handle names
 Route::resource('customers', CustomersController::class);
 Route::get('contact',[ContactFormController::class,'create'])->name('contact.create');
 Route::post('contact',[ContactFormController::class,'store'])->name('contact.store');
-
 Auth::routes();
-// Form 1
 Route::get('/home', [HomeController::class, 'index'])->name('home');
